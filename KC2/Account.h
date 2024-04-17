1633dc5eaba6aaee4f4b2292befe6e67be339312
Amalia <Amalia@DESKTOP-1353D5I>
2024-04-17 12:32:06 +0300

    Добавьте файлы проекта.

---
+++ KC2/Account.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "AccountHolder.h"
+#include <vector>
+class Account
+{
+private:
+	AccountHolder user;
+	string account_date, account_time;
+	double accountMoney;
+
+public:
+	Account(AccountHolder);
+	Account(AccountHolder, double);
+	void setMoney(double);
+	double getMoney() const;
+	string getUserName()const;
+};
